9805d515b3661b9c160afa912a12058529fd148d
g3tr3ktx <78469550+g3tr3ktx@users.noreply.github.com>
2022-05-03 07:36:46 +0530

    Fixed types

--- components/layouts/dashboard/DashboardLayout.mocks.ts
+++
@@ -1,9 +0,0 @@
-import { IDashboardLayout } from './DashboardLayout';
-
-const base: IDashboardLayout = {
-  sampleTextProp: 'Hello world!',
-};
-
-export const mockBaseTemplateProps = {
-  base,
-};
