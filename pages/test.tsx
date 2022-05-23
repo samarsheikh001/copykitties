79785025fa327082eee0659c421696f0bf19c6ec
g3tr3ktx <78469550+g3tr3ktx@users.noreply.github.com>
2022-05-23 20:26:24 +0530

    danica's b

--- pages/test.tsx
+++ pages/test.tsx
@@ -4,47 +4,8 @@ import SiteLogo from "../components/common/SiteLogo";
 export default function Example() {
   return (
     <div className="h-screen bg-red-500 flex flex-col overflow-hidden">
-     <div className="bg-yellow-200 flex-1 overflow-auto">
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
-       <div>samar</div>
+     <div className="bg-white flex-1 overflow-auto cursor-grab ">
+      Danica's bo<span className="cursor-grabbing">o</span>bs
      </div>
      <button>HEY</button>
     </div>
