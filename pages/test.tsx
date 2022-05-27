eb402e99ae5141ff6c3e84f2cb62e67591d82beb
g3tr3ktx <78469550+g3tr3ktx@users.noreply.github.com>
2022-05-27 10:51:37 +0530

    Fix error

--- pages/test.tsx
+++ pages/test.tsx
@@ -5,7 +5,7 @@ export default function Example() {
   return (
     <div className="h-screen bg-red-500 flex flex-col overflow-hidden">
      <div className="bg-white flex-1 overflow-auto cursor-grab ">
-      Danica's bo<span className="cursor-grabbing">o</span>bs
+      {"Danica's"} bo<span className="cursor-grabbing">o</span>bs
      </div>
      <button>HEY</button>
     </div>
